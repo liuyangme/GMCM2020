92374c1319facfea49473ef8198e209e77fdb24d
liuyang9609 <ly_0913@foxmail.com>
2020-07-23 11:36:31 +0800

    update

---
+++ floyd_func.m
@@ -0,0 +1,35 @@
+function [dist,mypath]=floyd_func(a,sb,db)
+% 输入： a—邻接矩阵(aij)是指 i 到 j 之间的距离，可以是有向的
+% sb—起点的标号； db—终点的标号
+% 输出： dist—最短路的距离； % mypath—最短路的路径
+    n=size(a,1); path=zeros(n);
+
+    for i=1:n
+        for j=1:n
+            if a(i,j)~=inf
+                path(i,j)=j; %j 是 i 的后续点
+            end
+        end
+    end
+
+    for k=1:n
+        for i=1:n
+            for j=1:n
+                if a(i,j)>a(i,k)+a(k,j)
+                    a(i,j)=a(i,k)+a(k,j);
+                    path(i,j)=path(i,k);
+                end
+            end
+        end
+    end
+
+    dist=a(sb,db);
+    mypath=sb; t=sb;
+    
+    while t~=db
+        temp=path(t,db);
+        mypath=[mypath,temp];
+        t=temp;
+    end
+    
+    return
