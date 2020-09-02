421caeb81b7f0e66896a687c96306901bc5f548f
Liu Yang <ly_0913@foxmail.com>
2020-09-02 21:46:12 +0800

    update

--- PaperRecurrence/2018F/Question2/Question2.m
+++ PaperRecurrence/2018F/Question2/Question2.m
@@ -1,10 +1,43 @@
-
+% 清除工作区
+% clear;clc;close all;
 % 创建决策变量
+
 load w;
 load tho;
+
+load('Da_Dl_Ia_Il.mat')
+load('PassengerNumber.mat')
+
 I=303;   %飞机数
 J=69;    %登机口数量
-Jt=28;  %航站楼登机口数量 （即1-28为航站楼登机口，29-69为卫星厅登机口）
+Jt=28;
+
+%Trans + 到达类型 + 出发类型 + 到达登机口 + 出发登机口
+% D:国内
+% I:国际
+% T:登机楼
+% S:卫星厅
+TransDDTT=15;
+TransDDTS=20;
+TransDDST=20;
+TransDDSS=15;
+
+TransDITT=35;
+TransDITS=40;
+TransDIST=40;
+TransDISS=35;
+
+TransIDTT=35;
+TransIDTS=40;
+TransIDST=40;
+TransIDSS=45;
+
+TransIITT=20;
+TransIITS=30;
+TransIIST=30;
+TransIISS=20;
+
+
 x = binvar(I,J);
 y = binvar(1,J);
 
@@ -12,8 +45,13 @@ y = binvar(1,J);
 % u = 0.005;   %权重（权重设置需要考量）
 u = 0.01;
 lamda = 100000;
-z = sum(x(:))-u*sum(y);  %目标函数
-z=-z;   %默认求解最小值，取反求最大
+
+z = (Da*Dl*Ta*Tl*TransDDTT+Da*Dl*Ta*Sl*TransDDTS+Da*Dl*Sa*Tl*TransDDST+Da*Dl*Sa*Sl*TransDDSS...
++Da*Il*Ta*Tl*TransDITT+Da*Il*Ta*Sl*TransDITS+Da*Il*Sa*Tl*TransDIST+Da*Il*Sa*Sl*TransDISS...
++Ia*Dl*Ta*Tl*TransIDTT+Ia*Dl*Ta*Sl*TransIDTS+Ia*Dl*Sa*Tl*TransIDST+Ia*Dl*Sa*Sl*TransIDSS...
++Ia*Il*Ta*Tl*TransIITT+Ia*Il*Ta*Sl*TransIITS+Ia*Il*Sa*Tl*TransIIST+Ia*Il*Sa*Sl*TransIISS)*PassengerNumber...
+-lamda*sum(x(:))+u*sum(y);  %目标函数
+
 
 tic
 %约束条件1---每架飞机最多停靠在一个登机口（可以不停靠在登机口，停靠在临时停靠区）
@@ -58,29 +96,30 @@ for m=1:I-1
 end
 toc
 
-%约束条件5---航班i是否在航站楼
+%约束条件5---是否在航站楼登机口
 tic
 c5=[];
 for i=1:I
     temp=0;
     for j=1:Jt
         temp=temp+x(i,j);
-    end 
-    c5=[c5;temp<=1]
+    end
+    c5=[c5;temp==Ti];
 end
 toc
 
-%约束条件6---航班i是否在卫星厅
+%约束条件6---是否在卫星厅登机口
 tic
 c6=[];
 for i=1:I
     temp=0;
-    for j=jt+1:J
+    for j=Jt+1:J
         temp=temp+x(i,j);
-    end 
-    c6=[c6;temp<=1]
+    end
+    c6=[c6;temp==Si];
 end
 toc
+
 
 C=[c1;c2;c3;c4;c5;c6];
 
